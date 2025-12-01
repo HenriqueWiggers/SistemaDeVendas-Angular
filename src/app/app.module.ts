205b4aea3fb7b7e86291698f747b17c222442900
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-01 10:08:55 -0300

    Criação do component de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProdutosModule } from './produtos';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProdutosModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/produtos/index.ts
+++ src/app/produtos/index.ts
@@ -1,1 +1,3 @@
 export * from './produtos.module';
+export * from './components';
+

79e20bdfb107868ba3ab005f10f4c9c5c52fffb7
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-03 20:25:06 -0300

    Implementando o service do cadastro de produtos

--- src/app/produtos/shared/produto-service.service.ts
+++ src/app/produtos/shared/produto-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { Produto } from './produto.model';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,8 +9,15 @@ export class ProdutoServiceService {
 
   constructor() { }
 
-  cadastrar(){
+  listarTodos(): Produto[]{
+    const produtos = localStorage['produtos'];
+    return produtos ? JSON.parse(produtos) : [];
+  }
 
+  cadastrar(produto:Produto){
+    const produtos = this.listarTodos();
+    produtos.push(produto)
+    localStorage['produtos'] = JSON.stringify(produtos);
   }
 
 }

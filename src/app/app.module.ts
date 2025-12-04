ae8ea46633541fb826f9d6fb452925c9c653b711
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-04 09:19:27 -0300

    Criação do modulo de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ProdutosModule } from './produtos';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { ClientesModule } from './clientes';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     ProdutosModule,
+    ClientesModule,
     RouterModule,
     AppRoutingModule
   ],

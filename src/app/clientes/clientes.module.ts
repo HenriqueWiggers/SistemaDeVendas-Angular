ce7f39d4bb0fb2d499a4204151ebddfdab326597
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-04 11:37:02 -0300

    funcao de cadastro funcionando

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastroClientesComponent } from './cadastro';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ClienteService } from './shared/cliente.service';
 
 
 
@@ -9,7 +12,13 @@ import { CadastroClientesComponent } from './cadastro';
     CadastroClientesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    ClienteService
   ]
+
 })
 export class ClientesModule { }

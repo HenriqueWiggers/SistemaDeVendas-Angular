f0c8cf4f66d7aa8593ab8827e38ecaf76962fd80
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-05 16:30:29 -0300

    Fazendo o html da tela de vendas

--- src/app/vendas/vendas-routing.ts
+++ src/app/vendas/vendas-routing.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
+import { CadastroVendaComponent } from "./cadastro";
 
 export const VendasRoutes: Routes = [
 
     {
-        
+        path: 'venda',
+        component: CadastroVendaComponent
     }
 
 ]

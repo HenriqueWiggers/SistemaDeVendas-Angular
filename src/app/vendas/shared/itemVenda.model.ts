360320d8046756b9373490a4d7a74e48c8b12aa8
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-08 11:49:36 -0300

    Implementado o model de venda e item venda

---
+++ src/app/vendas/shared/itemVenda.model.ts
@@ -0,0 +1,19 @@
+import { Venda } from "./vendas.model";
+import { Produto } from "src/app/produtos";
+
+export class itemVenda{
+
+
+
+    public vlr_subtotal: number
+
+    constructor(
+        public venda: Venda,
+        public produto:Produto,
+        public qtd_produto:number,
+        
+    ){
+        this.vlr_subtotal = this.qtd_produto * this.produto.vlr_unitario;
+    }
+
+}
--- src/app/vendas/shared/vendas.model.ts
+++ src/app/vendas/shared/vendas.model.ts
@@ -1,9 +1,11 @@
 import { Produto } from "src/app/produtos";
+import { itemVenda } from "./itemVenda.model";
 
 export class Venda {
     
     constructor(
-        public num_venda?:number
+        public num_venda?:number,
+        public itemVenda?: itemVenda[]
 
     ){
 
